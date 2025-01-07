58a2486af535d6ab89ba3971e09b209fc994151a
Jul Lang <jullanggit@proton.me>
2025-01-07 16:57:17 +0100

    String -> &str in Manager

--- src/configs.rs
+++ src/configs.rs
@@ -1,20 +1,22 @@
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-struct Config {
-    managers: Vec<Manager>,
+#[serde(deny_unknown_fields)]
+struct Config<'a> {
+    #[serde(borrow)]
+    managers: Vec<Manager<'a>>,
 }
 
 #[derive(Deserialize)]
-struct Manager {
+struct Manager<'a> {
     /// Command for adding one/multiple item
-    add: String,
+    add: &'a str,
     /// Whether multiple packages can be added at once
     mulit_add: bool,
     /// Command for adding an item
-    remove: String,
+    remove: &'a str,
     /// Command for upgrading all items
-    upgrade: Option<String>,
+    upgrade: Option<&'a str>,
     /// Command for getting a list of all installed items
-    installed: Option<String>,
+    installed: Option<&'a str>,
 }
