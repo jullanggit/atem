71afc9d3f355cdfce53dadd77c9c99073d241354
Jul Lang <jullanggit@proton.me>
2025-01-08 10:16:09 +0100

    Change &str to String for simplicity

--- src/configs.rs
+++ src/configs.rs
@@ -2,26 +2,25 @@ use serde::Deserialize;
 
 #[derive(Deserialize)]
 #[serde(deny_unknown_fields)]
-struct Config<'a> {
-    #[serde(borrow)]
-    managers: Vec<Manager<'a>>,
+struct Config {
+    managers: Vec<Manager>,
 }
 
 #[derive(Deserialize)]
 #[serde(deny_unknown_fields)]
-struct Manager<'a> {
+struct Manager {
     /// Command for adding one/multiple item
-    add: &'a str,
+    add: String,
     /// Whether multiple packages can be added at once
     #[serde(default)]
     single_add: bool,
     /// Command for adding an item
-    remove: &'a str,
+    remove: String,
     /// Command for getting a list of all installed items
-    installed: &'a str,
+    installed: String,
     /// Command for upgrading all items
-    upgrade: Option<&'a str>,
+    upgrade: Option<String>,
     /// The items the manager is supposed to have
     #[serde(default)]
-    items: Vec<&'a str>,
+    items: Vec<String>,
 }
