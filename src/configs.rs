b0c2678b1396ca06a97276184d2f6b328f3169e7
Jul Lang <jullanggit@proton.me>
2025-01-08 14:59:53 +0100

    Change managers to a Hashmap, move load_managers() into Config and extract CONFIG_PATH into a const LazyCell

--- src/configs.rs
+++ src/configs.rs
@@ -1,11 +1,47 @@
-use std::{fs, path::PathBuf};
+use std::{cell::LazyCell, collections::HashMap, fs, path::PathBuf};
 
 use serde::Deserialize;
 
-#[derive(Deserialize)]
-#[serde(deny_unknown_fields)]
+// I think this is fine
+#[expect(clippy::declare_interior_mutable_const)]
+const CONFIG_PATH: LazyCell<String> = LazyCell::new(|| {
+    let home = std::env::var("HOME").expect("HOME is not set");
+    format!("{home}/.config/meta")
+});
+
 struct Config {
-    managers: Vec<Manager>,
+    managers: HashMap<String, Manager>,
+}
+impl Config {
+    // The const in question is a LazyCell, so its contents dont change during the execution of the
+    #[expect(clippy::borrow_interior_mutable_const)]
+    fn load_managers() -> Self {
+        let manager_path = PathBuf::from(format!("{}/managers", *CONFIG_PATH));
+
+        let mut managers = HashMap::new();
+        manager_path
+            .read_dir()
+            .expect("Failed to read manager dir")
+            .map(|manager_file| manager_file.unwrap())
+            .for_each(|manager_file| {
+                let manager_string =
+                    fs::read_to_string(manager_file.path()).expect("Failed to read manager file");
+                let mut manager: Manager =
+                    toml::from_str(&manager_string).expect("Failed to deserialize manager");
+
+                managers.insert(
+                    manager_file
+                        .file_name()
+                        .to_str()
+                        .expect("Failed to get manager name")
+                        .into(),
+                    manager,
+                );
+            });
+
+        Self { managers: managers }
+    }
+    fn load_items(&mut self) {}
 }
 
 #[derive(Deserialize)]
@@ -25,32 +61,4 @@ struct Manager {
     /// The items the manager is supposed to have
     #[serde(default)]
     items: Vec<String>,
-    /// The name of the manager
-    #[serde(default)]
-    name: String,
-}
-
-fn load_managers() -> Config {
-    let home = std::env::var("HOME").expect("HOME is not set");
-    let manager_path = PathBuf::from(format!("{home}/.config/meta/managers"));
-
-    Config {
-        managers: manager_path
-            .read_dir()
-            .expect("Failed to read manager dir")
-            .map(|manager_file| manager_file.unwrap())
-            .map(|manager_file| {
-                let manager_string =
-                    fs::read_to_string(manager_file.path()).expect("Failed to read manager file");
-                let mut manager: Manager =
-                    toml::from_str(&manager_string).expect("Failed to deserialize manager");
-                manager.name = manager_file
-                    .file_name()
-                    .to_str()
-                    .expect("Failed to get manager name")
-                    .into();
-                manager
-            })
-            .collect(),
-    }
 }
