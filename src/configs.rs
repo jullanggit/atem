6b1ce6d17e89c4e740cab7148ffac9e22831b7d3
Jul Lang <jullanggit@proton.me>
2025-01-08 10:16:09 +0100

    Add load_managers()

--- src/configs.rs
+++ src/configs.rs
@@ -1,3 +1,5 @@
+use std::{fs, path::PathBuf};
+
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -23,4 +25,32 @@ struct Manager {
     /// The items the manager is supposed to have
     #[serde(default)]
     items: Vec<String>,
+    /// The name of the manager
+    #[serde(default)]
+    name: String,
+}
+
+fn load_managers() -> Config {
+    let home = std::env::var("HOME").expect("HOME is not set");
+    let manager_path = PathBuf::from(format!("{home}/.config/meta/managers"));
+
+    Config {
+        managers: manager_path
+            .read_dir()
+            .expect("Failed to read manager dir")
+            .map(|manager_file| manager_file.unwrap())
+            .map(|manager_file| {
+                let manager_string =
+                    fs::read_to_string(manager_file.path()).expect("Failed to read manager file");
+                let mut manager: Manager =
+                    toml::from_str(&manager_string).expect("Failed to deserialize manager");
+                manager.name = manager_file
+                    .file_name()
+                    .to_str()
+                    .expect("Failed to get manager name")
+                    .into();
+                manager
+            })
+            .collect(),
+    }
 }
