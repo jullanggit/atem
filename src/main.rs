f8f8a343816a7333c81fb8c551e91e158a6a0476
Jul Lang <jullanggit@proton.me>
2025-01-11 12:54:20 +0100

    add config loading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use clap::Parser;
-use cli::Cli;
+// Dont want to slap this on every function that uses CONFIG_PATH
+// Look at the reasoning there for why this is ok
+#![expect(clippy::borrow_interior_mutable_const)]
+
+use serde::Deserialize;
+use std::{cell::LazyCell, collections::HashMap, fs, path::PathBuf, process::exit};
+use toml::Table;
 
 mod cli;
 
@@ -10,7 +15,7 @@ const CONFIG_PATH: LazyCell<String> = LazyCell::new(|| {
     format!("{home}/.config/meta")
 });
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 #[serde(deny_unknown_fields)]
 struct Manager {
     /// Command for adding one/multiple item
@@ -33,6 +38,41 @@ struct Manager {
 
 fn main() {
     let mut managers = get_managers();
+
+    let configs_path = PathBuf::from(format!("{}/configs", *CONFIG_PATH));
+    configs_path
+        .read_dir()
+        .expect("Failed to read configs dir")
+        .for_each(|config_file| {
+            let config_file = config_file.unwrap();
+
+            let config_string =
+                fs::read_to_string(config_file.path()).expect("Failed to read config file");
+
+            let config_table: Table =
+                toml::from_str(&config_string).expect("Failed to deserialize config");
+
+            for (manager_name, value) in config_table {
+                let manager = managers
+                    .get_mut(&manager_name)
+                    .expect("Manager should exist");
+
+                if let Some(array) = value.as_array() {
+                    for item in array {
+                        manager
+                            .items
+                            .push(item.as_str().expect("Item should be a string").to_string());
+                    }
+                // We allow single-item entries
+                } else if let Some(item) = value.as_str() {
+                    manager.items.push(item.to_string());
+                } else {
+                    eprintln!("Items should be either a single string or an array of string");
+                    exit(1);
+                }
+            }
+        });
+}
 
 fn get_managers() -> HashMap<String, Manager> {
     let manager_path = PathBuf::from(format!("{}/managers", *CONFIG_PATH));
