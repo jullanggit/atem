6131b5d6d84a20a184ea157e26976ce64f98069e
Jul Lang <jullanggit@proton.me>
2025-01-14 21:16:58 +0100

    reorder code

--- src/main.rs
+++ src/main.rs
@@ -65,110 +65,6 @@ fn main() {
     }
 }
 
-/// Takes a formatted command (containing <item> or <items>) and runs it with the provided items
-fn fmt_run_command(format_command: &str, items: &[String]) {
-    // Only add one item at a time
-    if format_command.contains("<item>") {
-        items
-            .iter()
-            .map(|item| format_command.replace("<item>", item))
-            .for_each(run_command);
-    // Add all items at once
-    } else if format_command.contains("<items>") {
-        let items = items.join(" "); // TODO: Maybe make the separator configurable
-        let command = format_command.replace("<items>", &items);
-        run_command(command);
-    } else {
-        eprintln!("Add command should contain either <item> or <items>");
-        exit(1);
-    };
-}
-
-/// Runs the given command using the shell
-fn run_command(command: String) {
-    let status = Command::new("fish")
-        .arg("-c")
-        .arg(command)
-        .status()
-        .expect("Failed to spawn child");
-
-    if !status.success() {
-        eprintln!("Command did not exit successfully");
-        exit(1);
-    }
-}
-
-/// Asks the user for confirmation. Returns the users answer
-fn ask_for_confirmation() -> bool {
-    println!("{}", "Continue?".bold());
-
-    let mut buf = String::new();
-
-    stdin().read_line(&mut buf).expect("Failed to get input");
-
-    match buf.trim() {
-        "y" | "Y" | "" => true, // newline is defaulted to y
-        _ => false,
-    }
-}
-
-/// Computes and prints the items to add and remove for each manager
-fn compute_add_remove(managers: &mut HashMap<String, Manager>) {
-    for (manager_name, manager) in managers {
-        // Get system items
-        let output = Command::new("fish").arg("-c").arg(&manager.list).output(); // TODO: Add setting for which shell to use
-
-        let system_items = match output {
-            Ok(output) => {
-                if output.status.success() {
-                    String::from_utf8(output.stdout).expect("Command output should be UTF-8")
-                } else {
-                    eprintln!(
-                        "Command 'list' for manager {manager_name} failed with error: \n{}",
-                        String::from_utf8_lossy(&output.stderr)
-                    );
-                    exit(1);
-                }
-            }
-            Err(e) => {
-                eprintln!("Failed to execute command 'list': {e}");
-                exit(1);
-            }
-        };
-
-        let system_items = system_items
-            .split_whitespace()
-            .map(str::to_string)
-            .collect();
-
-        manager.items_to_add = manager
-            .items
-            .difference(&system_items)
-            .map(Clone::clone)
-            .collect();
-        manager.items_to_remove = system_items
-            .difference(&manager.items)
-            .map(Clone::clone)
-            .collect();
-    }
-}
-
-/// Prints all items to remove/add
-fn print_diff(managers: &HashMap<String, Manager>) {
-    for (manager_name, manager) in managers {
-        // If are any items to add/remove
-        if !manager.items_to_add.is_empty() | !manager.items_to_remove.is_empty() {
-            println!("{}:", manager_name.bold());
-            for item_to_add in &manager.items_to_add {
-                println!("{}", item_to_add.green());
-            }
-            for item_to_remove in &manager.items_to_remove {
-                println!("{}", item_to_remove.red());
-            }
-        }
-    }
-}
-
 fn load_managers() -> HashMap<String, Manager> {
     let manager_path = PathBuf::from(format!("{}/managers", *CONFIG_PATH));
 
@@ -252,5 +148,109 @@ fn load_configs(managers: &mut HashMap<String, Manager>) {
         }
 
         i = i.strict_add(1); // i += 1
+    }
+}
+
+/// Computes and prints the items to add and remove for each manager
+fn compute_add_remove(managers: &mut HashMap<String, Manager>) {
+    for (manager_name, manager) in managers {
+        // Get system items
+        let output = Command::new("fish").arg("-c").arg(&manager.list).output(); // TODO: Add setting for which shell to use
+
+        let system_items = match output {
+            Ok(output) => {
+                if output.status.success() {
+                    String::from_utf8(output.stdout).expect("Command output should be UTF-8")
+                } else {
+                    eprintln!(
+                        "Command 'list' for manager {manager_name} failed with error: \n{}",
+                        String::from_utf8_lossy(&output.stderr)
+                    );
+                    exit(1);
+                }
+            }
+            Err(e) => {
+                eprintln!("Failed to execute command 'list': {e}");
+                exit(1);
+            }
+        };
+
+        let system_items = system_items
+            .split_whitespace()
+            .map(str::to_string)
+            .collect();
+
+        manager.items_to_add = manager
+            .items
+            .difference(&system_items)
+            .map(Clone::clone)
+            .collect();
+        manager.items_to_remove = system_items
+            .difference(&manager.items)
+            .map(Clone::clone)
+            .collect();
+    }
+}
+
+/// Prints all items to remove/add
+fn print_diff(managers: &HashMap<String, Manager>) {
+    for (manager_name, manager) in managers {
+        // If are any items to add/remove
+        if !manager.items_to_add.is_empty() | !manager.items_to_remove.is_empty() {
+            println!("{}:", manager_name.bold());
+            for item_to_add in &manager.items_to_add {
+                println!("{}", item_to_add.green());
+            }
+            for item_to_remove in &manager.items_to_remove {
+                println!("{}", item_to_remove.red());
+            }
+        }
+    }
+}
+
+/// Asks the user for confirmation. Returns the users answer
+fn ask_for_confirmation() -> bool {
+    println!("{}", "Continue?".bold());
+
+    let mut buf = String::new();
+
+    stdin().read_line(&mut buf).expect("Failed to get input");
+
+    match buf.trim() {
+        "y" | "Y" | "" => true, // newline is defaulted to y
+        _ => false,
+    }
+}
+
+/// Takes a formatted command (containing <item> or <items>) and runs it with the provided items
+fn fmt_run_command(format_command: &str, items: &[String]) {
+    // Only add one item at a time
+    if format_command.contains("<item>") {
+        items
+            .iter()
+            .map(|item| format_command.replace("<item>", item))
+            .for_each(run_command);
+    // Add all items at once
+    } else if format_command.contains("<items>") {
+        let items = items.join(" "); // TODO: Maybe make the separator configurable
+        let command = format_command.replace("<items>", &items);
+        run_command(command);
+    } else {
+        eprintln!("Add command should contain either <item> or <items>");
+        exit(1);
+    };
+}
+
+/// Runs the given command using the shell
+fn run_command(command: String) {
+    let status = Command::new("fish")
+        .arg("-c")
+        .arg(command)
+        .status()
+        .expect("Failed to spawn child");
+
+    if !status.success() {
+        eprintln!("Command did not exit successfully");
+        exit(1);
     }
 }
